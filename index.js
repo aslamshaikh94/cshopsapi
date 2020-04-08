236960f5b8453875f522c0b2033af39d74bdd9bb
aslamshaikh94 <highflyingaslam@gmail.com>
2020-04-08 23:20:07 +0530

    Changes

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-let express = require('express');
-const path = require('path');
-require('dotenv').config();
-const app = express();
-
-const PORT = process.env.PORT || 8080
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-    next();
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-var userauth = require('./routes/auth');
-var product = require('./routes/product');
-var enquiry = require('./routes/enquiry');
-var users = require('./routes/users');
-var addto = require('./routes/addto');
-var myprofile = require('./routes/myprofile');
-var orders = require('./routes/orders');
-
-app.use("/auth", userauth);
-app.use("/users", users);
-app.use("/product", product);
-app.use("/enquiry", enquiry);
-app.use("/addto", addto);
-app.use("/myprofile", myprofile);
-app.use("/orders", orders);
-
-// let port = process.env.APP_PORT
-// app.listen(port, ()=>{
-//   console.log("server is runing " + port)
-// });
-
-
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('index'))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
