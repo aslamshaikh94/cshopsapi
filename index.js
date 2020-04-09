b3b22011fb3f7538b6ae720f4c8a28c71da213c8
aslamshaikh94 <highflyingaslam@gmail.com>
2020-04-09 14:08:44 +0530

    Changes

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const path = require('path');
 // require('dotenv').config();
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-    next();
-});
 
 
 let PORT = process.env.APP_PORT || 5000
