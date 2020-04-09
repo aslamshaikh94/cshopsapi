df0c28aee422b1c917f8544462441e592a8c6d66
aslamshaikh94 <highflyingaslam@gmail.com>
2020-04-09 14:26:39 +0530

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin: http://localhost:3000/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
     next();
 });
