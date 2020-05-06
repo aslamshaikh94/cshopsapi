6ed9dac874a602a03c4df7ea6bc04b161fb71b74
aslamshaikh94 <highflyingaslam@gmail.com>
2020-05-07 04:26:59 +0530

    Changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Credentials', false);
+    res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth-token");
     next();
