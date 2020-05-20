ea7e28e4be4d608ae5b421645dcbf44321271439
aslamahmed1494 <aslamahmed1494@gmail.com>
2020-05-20 17:43:09 +0530

    Changes

--- routes/auth.js
+++ routes/auth.js
@@ -94,77 +94,18 @@ app.post('/login', (req, res, next)=>{
 });
 
 
-// for facebaook
-// passport.use(new FacebookStrategy({
-//     clientID: process.env.FACEBOOK_APP_ID,
-//     clientSecret: process.env.FACEBOOK_APP_SECRET,    
-//     callbackURL: "/auth/facebook/callback",
-//     profileFields: ['id', 'displayName', 'photos', 'email']
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     const {email, name, picture} = profile._json
-//     let user = {
-//       fname:name,
-//       lname:'',
-//       usertype:'',      
-//       email:email,
-//     }
-//     let sqlSELECT = `SELECT * FROM users WHERE email='${email}'`
-//     let sqlINSERT = 'INSERT INTO users SET ?'
-//     connection.query(sqlSELECT, (err, result, fields)=>{
-//       if(result.length>0){        
-//         return done(null, result[0]);
-//       }
-//       else{
-//         connection.query(sqlINSERT, user, function (err, result, fields) {          
-//           done(null, user);
-//         });
-//       }
-//     })
-//   }
-// ));
-
-
-
-// app.get('/facebook/callback',
-//   passport.authenticate('facebook', { successRedirect: '/auth',
-//                                       failureRedirect: '/login' 
-//                                     }), (req, res)=> {
-//   // Successful authentication, redirect home.    
-//   console.log('Successful')
-//   res.redirect('http://localhost:3000');
-// });
-
-// app.get('/facebook/callback', function(req, res, next) {
-//     passport.authenticate('facebook', function(err, user, info) {
-//         let {id, usertype} = user;
-//         jwt.sign({id, usertype}, SECRETKEY, /*{ expiresIn: 3600 },*/ (err, token)=>{
-//           if(err) res.json({error:err})
-//           res.json({
-//             status:true,
-//             id,
-//             usertype,
-//             token
-//           });
-//         });        
-//         next()
-//     })(req, res, next);
-// });
-
-
-
 passport.use('facebookToken', new FacebookTokenStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
   passReqToCallback: true
-}, async (req, accessToken, refreshToken, profile, done) => {
+}, async (req, accessToken, refreshToken, profile, done) => {  
   try {
     const {email, name, picture} = profile._json
     // console.log(profile)
     let user = {
       fname:name,
       lname:'',
-      usertype:'',      
+      usertype:req.query.type,      
       email:email,
     }
     let sqlSELECT = `SELECT * FROM users WHERE email='${email}'`
@@ -180,18 +121,12 @@ passport.use('facebookToken', new FacebookTokenStrategy({
           })
         });
       }
-    })
-    // console.log('profile', profile);
-    // console.log('accessToken', accessToken);
-    // console.log('refreshToken', refreshToken);       
+    })     
   } catch(error) {
     done(error, false, error.message);
   }
 }));
-// passport.authenticate('facebookToken', {}),
 
-
-// app.post('/facebook', passport.authenticate('facebookToken', {}), facebookOAuth);
 
 
 app.post('/facebook', function(req, res, next) {
@@ -211,21 +146,5 @@ app.post('/facebook', function(req, res, next) {
     })(req, res, next);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/facebook/logout', passport.authenticate('facebookToken', {}), (req, res)=>{  
-  req.logout();
-  res.redirect('http://localhost:3000/auth');
-});
 
 module.exports = app;
